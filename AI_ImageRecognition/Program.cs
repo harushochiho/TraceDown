f1e3a1ea768082159bfb36b3e8fa254090e415e7
harusho <harusho@pm.me>
2026-05-02 18:37:42 -0400

    Added deepseek for recognition.

--- AI_ImageRecognition/Program.cs
+++ AI_ImageRecognition/Program.cs
@@ -1,5 +1,6 @@
-using System.Net.Mime;
+using System.Diagnostics;
 using System.Text;
+using System.Text.Json;
 using AI_ImageRacognition;
 using Microsoft.Extensions.AI;
 using OllamaSharp;
@@ -30,43 +31,45 @@ app.MapGet("/aicalling",
            {
                var deepseekChatClient = app.Services.GetKeyedService<IChatClient>("deepseek");
                var llamaChatClient    = app.Services.GetKeyedService<IChatClient>("llama");
-
+               var deepseekPrompt = File.ReadAllText("deepseek_prompt.txt",
+                                                      Encoding.UTF8);
+               var llamaPrompt = File.ReadAllText("llama_prompt.txt",
+                                                   Encoding.UTF8);
+               var systemPrompt = File.ReadAllText("system_prompt.txt",
+                                                    Encoding.UTF8);
+               Debug.Print($"Deepseek prompt: \n{deepseekPrompt}\n");
+               Debug.Print($"Llama prompt: \n{llamaPrompt}\n");
+               Debug.Print($"System prompt: \n{systemPrompt}\n");
                // var message = new ChatMessage(ChatRole.User,
-               //                               """Extract all line items from this receipt. Respond in JSON format with this structure:{'CampanyName':'Company Name','items': [{ 'name': 'English item name(Chinese item name)', 'quantity': 1.500, 'unitPrice': 0.00, 'totalPrice': 0.00, 'Category': 'category', 'OnSale':true, 'Taxable':true }],  'subtotal': 0.00 }""");
-               var deepseek_message = new ChatMessage(ChatRole.User,
-                                                      File.ReadAllText("deepseek_prompt.txt",
-                                                                       Encoding.UTF8));
-               var llama_message = new ChatMessage(ChatRole.User,
-                                                   File.ReadAllText("llama_prompt.txt",
-                                                                    Encoding.UTF8));
+               //                               """Extract all line items from this receipt. Respond in JSON format with this structure:{'CompanyName':'Company Name','items': [{ 'name': 'English item name(Chinese item name)', 'quantity': 1.500, 'unitPrice': 0.00, 'totalPrice': 0.00, 'Category': 'category', 'OnSale':true, 'Taxable':true }],  'subtotal': 0.00 }""");
+               var deepseekMessage = new ChatMessage(ChatRole.User,
+                                                      deepseekPrompt);
+               var llamaMessage = new ChatMessage(ChatRole.User,
+                                                   llamaPrompt);
                var systemMessage = new ChatMessage(ChatRole.System,
-                                                   File.ReadAllText("system_prompt.txt",
-                                                                    Encoding.UTF8));
+                                                   systemPrompt);
                byte[] receiptBytes = File.ReadAllBytes("receipt.png");
 
-               deepseek_message.Contents.Add(new DataContent(receiptBytes,
+               deepseekMessage.Contents.Add(new DataContent(receiptBytes,
                                                              "image/png"));
 
                //var response = await chatClient.GetResponseAsync<Receipt>([systemMessage, message], new ChatOptions{ Temperature = 0});
-               var deepseekResponse = await deepseekChatClient.GetResponseAsync([deepseek_message],
+               var deepseekResponse = await deepseekChatClient.GetResponseAsync([deepseekMessage],
                                                                                 new ChatOptions { Temperature = 0 });
-               llama_message.Contents.Add(new TextContent(deepseekResponse.Text));
-               llama_message.Contents.Add(new DataContent(receiptBytes,
+               File.AppendAllText("deepseek_response.txt",
+                                       JsonSerializer.Serialize(deepseekResponse) + "\n");
+               llamaMessage.Contents.Add(new TextContent(deepseekResponse.Text));
+               llamaMessage.Contents.Add(new DataContent(receiptBytes,
                                                           "image/png"));
-               var llamaResponse = await llamaChatClient.GetResponseAsync<Receipt>([systemMessage, llama_message],
+               var llamaResponse = await llamaChatClient.GetResponseAsync<Receipt>([systemMessage, llamaMessage],
                                                                                    new ChatOptions { Temperature = 0 });
-
+                
                // var response = await chatClient.GetStreamingResponseAsync([systemMessage, message],
                //                                                           new ChatOptions { Temperature = 0 }).StreamToEndAsync().WaitAsync(TimeSpan.FromMinutes(30));
-               return System.Text.Json.JsonSerializer.Serialize(llamaResponse);
+               File.AppendAllText("llama_response.txt",
+                                  JsonSerializer.Serialize(llamaResponse)+ "\n");
+               return JsonSerializer.Serialize(llamaResponse);
            })
    .WithName("AI Calling");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date,
-                       int      TemperatureC,
-                       string?  Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
