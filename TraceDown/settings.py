9da4de373870b6f3fdb6705eeaba821eb3d839f6
harusho <harusho@pm.me>
2026-05-15 19:55:24 -0400

    Removed api_calling.txt; Added api_calling.txt to .gitignore;

--- TraceDown/settings.py
+++ TraceDown/settings.py
@@ -127,5 +127,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Ollama settings
-OLLAMA_IN = 'http://ollama:11434'
-OLLAMA_OUT = 'http://localhost:8833'
+AI_CALLING_IN = 'http://ollama:11434'
+AI_CALLING_OUT = 'http://localhost:8833'
--- main/services/image_recognition.py
+++ main/services/image_recognition.py
@@ -1,7 +1,6 @@
 import base64
 import json
 import os
-from hmac import new
 from pathlib import Path
 from zoneinfo import ZoneInfo
 
@@ -18,7 +17,7 @@ file_path = BASE_DIR / "docs"
 
 
 def image_recognition(image_encoded) -> str:
-    file_readings = FilesProcessing();
+    file_readings = FilesProcessing()
 
     file_readings.read_files(
         [f"{file_path}/deepseek_prompt.txt", f"{file_path}/llama_prompt.txt", f"{file_path}/system_prompt.txt"])
@@ -27,21 +26,18 @@ def image_recognition(image_encoded) -> str:
 
     docker_running = os.getenv("DOCKER_RUNNING", "False").lower() == "true"
 
-    ollama_host = os.getenv("OLLAMA_IN") if docker_running else os.getenv("OLLAMA_OUT")
+    ollama_host = os.getenv("AI_CALLING_IN") if docker_running else os.getenv("AI_CALLING_OUT")
 
     if not ollama_host:
-        ollama_host = getattr(settings, "OLLAMA_IN") if docker_running else getattr(settings, "OLLAMA_OUT")
+        ollama_host = getattr(settings, "AI_CALLING_IN") if docker_running else getattr(settings, "AI_CALLING_OUT")
 
     print(f"DOCKER_RUNNING: {docker_running}, ollama_host: {ollama_host}")
     timezone.activate(ZoneInfo("America/Toronto"))
     ollama_client = Client(ollama_host, timeout=httpx.Timeout(900.0))
+
     # Call the DeepSeek API to get the image description
     deepseek_messages = [{'role': 'user', 'content': deepseek_prompt, 'images': [image_encoded]}]
     deepseek_response: ChatResponse = ollama_client.chat(model="deepseek-ocr:latest", messages=deepseek_messages)
-
-    # with open(f"{file_path}/deepseek_response.txt", "a", encoding="utf-8") as file:
-    #     print(f"{timezone.localtime().strftime("%Y-%m-%d %H:%M:%S")} Logging deepseek_response.txt")
-    #     file.write(f"{timezone.localtime().strftime("%Y-%m-%d %H:%M:%S")}-{deepseek_response.model_dump_json()}\n")
 
     FilesProcessing.write_files(
         path=f"{file_path}/deepseek_response.txt", content=deepseek_response.model_dump_json(),
@@ -63,22 +59,20 @@ def image_recognition(image_encoded) -> str:
 
     llama_response: ChatResponse = llama_client.chat(model='llama3.2-vision:latest', messages=llama_prompt_messages)
 
-    # with open(f"{file_path}/llama_response.txt", "a", encoding="utf-8") as file:
-    #     print(f"{timezone.localtime().strftime("%Y-%m-%d %H:%M:%S")} Logging llama_response.txt")
-    #     file.write(f"{timezone.localtime().strftime("%Y-%m-%d %H:%M:%S")}-{llama_response.model_dump_json()}\n")
     FilesProcessing.write_files(
         path=f"{file_path}/llama_response.txt", content=llama_response.model_dump_json(),
         time=timezone.localtime().strftime("%Y-%m-%d %H:%M:%S"), line_end="\n")
 
     llama_client.close()
-    
+
     if not llama_response.message.content:
         raise ValueError("AI response is empty.")
-    
+
     prefix_index = llama_response.message.content.find("{")
     suffix_index = llama_response.message.content.rfind("}")
 
-    result = llama_response.message.content[prefix_index:suffix_index + 1] if prefix_index != -1 and suffix_index != -1 else llama_response.message.content
+    result = llama_response.message.content[
+        prefix_index:suffix_index + 1] if prefix_index != -1 and suffix_index != -1 else llama_response.message.content
 
     return result
 
@@ -88,7 +82,7 @@ def call_api_localhost(image):
     image_data = base64.b64encode(image.read()).decode('utf-8')
     media_type = getattr(image, 'content_type', 'image/png')
 
-    file_readings = FilesProcessing();
+    file_readings = FilesProcessing()
 
     file_readings.read_files([f"{file_path}/deepseek_prompt.txt"])
 
